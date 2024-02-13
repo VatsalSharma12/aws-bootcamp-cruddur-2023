04ef418d89131caf22f8fcaf11a354784a1f7106
VatsalSharma12 Sharma <sharma.vatsal6@gmail.com>
2024-02-13 14:07:25 +0000

    notifications.js css

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage'; 
-import NotificationsFeedPage from './pages/NotificationsFeedPage'
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -18,10 +18,11 @@ import {
 
 const router = createBrowserRouter([
   {
-    path: "/notifications",
-    element: <HomeFeedPage />
-  }, {
     path: "/",
+    element: <HomeFeedPage />
+  }, 
+  {
+    path: "/notifications",
     element: <NotificationsFeedPage />
   },
   {
