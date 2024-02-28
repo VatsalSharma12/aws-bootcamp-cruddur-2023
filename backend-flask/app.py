e57eef03c87bf6bf6bc027d89085e92a212d6ac5
VatsalSharma12 <158792594+VatsalSharma12@users.noreply.github.com>
2024-02-28 09:01:44 +0000

    implementing cloud watch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -23,8 +23,24 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor 
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter,SimpleSpanProcessor
 # xray ----
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+# from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.ext.flask.middleware import XRayMiddleware 
+
+# Cloudwatch Logs ---
+import watchtower
+import logging
+from time import strftime 
+
+# Configuring Logger to Use CloudWatch
+# LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
+# LOGGER.info("some message") 
+# LOGGER.info("test log") 
+
 # Honeycomb -----
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -44,8 +60,8 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)  
-
-XRayMiddleware(app, xray_recorder) 
+# xray 
+# XRayMiddleware(app, xray_recorder) 
 
 # Honeycomb ---
 # Initialize automatic instrumentation with Flask
@@ -61,7 +77,13 @@ cors = CORS(
   expose_headers="location,link",
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
-)
+) 
+# Watchtower ----
+#@app.after_request
+# def after_request(response):
+#    timestamp = strftime('[%Y-%b-%d %H:%M]')
+#    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#   return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
