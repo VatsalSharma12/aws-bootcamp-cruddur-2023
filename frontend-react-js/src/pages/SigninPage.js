6f14e396b65c25bce279e211d33d08131c4111c7
VatsalSharma12 Sharma <sharma.vatsal6@gmail.com>
2024-03-04 12:25:42 +0000

    sign in page changed

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -1,40 +1,43 @@
 import './SigninPage.css';
-import React from "react";
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
+import React, { useState } from "react";
+import { ReactComponent as Logo } from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
-
-// [TODO] Authenication
-import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify';
 
 export default function SigninPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [cognitoErrors, setCognitoErrors] = useState('');
 
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [errors, setErrors] = React.useState('');
-
-  const onsubmit = async (event) => {
+  const onSubmit = async (event) => {
+    setCognitoErrors('');
     event.preventDefault();
-    setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
+    try {
+      Auth.signIn(email, password)
+        .then(user => {
+          localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken);
+          window.location.href = "/";
+        })
+        .catch(err => { console.log('Error!', err); });
+    } catch (error) {
+      if (error.code === 'UserNotConfirmedException') {
+        window.location.href = "/confirm";
+      }
+      setCognitoErrors(error.message);
     }
-    return false
   }
 
-  const email_onchange = (event) => {
+  const emailOnChange = (event) => {
     setEmail(event.target.value);
   }
-  const password_onchange = (event) => {
+
+  const passwordOnChange = (event) => {
     setPassword(event.target.value);
   }
 
-  let el_errors;
-  if (errors){
-    el_errors = <div className='errors'>{errors}</div>;
+  let errorElement;
+  if (cognitoErrors) {
+    errorElement = <div className='errors'>{cognitoErrors}</div>;
   }
 
   return (
@@ -43,9 +46,9 @@ export default function SigninPage() {
         <Logo className='logo' />
       </div>
       <div className='signin-wrapper'>
-        <form 
+        <form
           className='signin_form'
-          onSubmit={onsubmit}
+          onSubmit={onSubmit}
         >
           <h2>Sign into your Cruddur account</h2>
           <div className='fields'>
@@ -54,7 +57,7 @@ export default function SigninPage() {
               <input
                 type="text"
                 value={email}
-                onChange={email_onchange} 
+                onChange={emailOnChange}
               />
             </div>
             <div className='field text_field password'>
@@ -62,25 +65,21 @@ export default function SigninPage() {
               <input
                 type="password"
                 value={password}
-                onChange={password_onchange} 
+                onChange={passwordOnChange}
               />
             </div>
           </div>
-          {el_errors}
+          {errorElement}
           <div className='submit'>
             <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
             <button type='submit'>Sign In</button>
           </div>
-
         </form>
         <div className="dont-have-an-account">
-          <span>
-            Don't have an account?
-          </span>
+          <span>Don't have an account?</span>
           <Link to="/signup">Sign up!</Link>
         </div>
       </div>
-
     </article>
   );
 }
